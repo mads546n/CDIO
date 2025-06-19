9f9137eaf0acf746c57793158500d694395f3dd2
Kielstrup <casper.kielstrup@hotmail.com>
2025-06-19 12:08:19 +0200

    Typo

--- pc_vision/vision.py
+++ pc_vision/vision.py
@@ -351,7 +351,7 @@ class VisionSystem:
             (ordered_box[2], ordered_box[3]),
             (ordered_box[3], ordered_box[0])
         ]
-        side_lenghts = [np.linag.norm(np.array(p2)-np.array(p1)) for (p1, p2) in sides]
+        side_lenghts = [np.linalg.norm(np.array(p2)-np.array(p1)) for (p1, p2) in sides]
         short_side_indices = sorted(range(4), key=lambda i: side_lenghts[i])[:2]
         goal_centers = [midpoint(*sides[i]) for i in short_side_indices]
 
